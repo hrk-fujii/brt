2b593c18d326a83c159c88b24a27849fffee1e7c
Hiroki Fujii <hfujii@hisystron.com>
2021-02-27 00:59:38 +0900

    トップページの基ビューを作成

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -2,15 +2,60 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Model\Dao\Informations;
+use Util\MailUtil;
+use Model\Dao\Users;
+use Model\Dao\Bento;
 
 
-// TOPページのコントローラ
+
+// トップページ表示
 $app->get('/', function (Request $request, Response $response) {
+    $bentoTable = new Bento($this->db);
+    // 日付の設定。今日販売の弁当がなければ最短販売日の弁当
+    $saleDate = strtotime(date("Y-m-d", time()));
+    $bentoArray = NULL;
+    $data["day"] = NULL;
+    for ($i=0; $i<=9; $i++){
+        $bentoArray = $bentoTable->selectFromStartSaleAt($saleDate, $saleDate + 60 * 60 * 24 - 1);
+        if (!empty($bentoArray) && count($bentoArray)!=0){
+            if ($i===0){
+                $data["day"] = "本日";
+            }elseif ($i===1){
+                $data["day"] = "明日";
+            }else{
+                $data["day"] = date("n月j日", $saleDate);
+            }
+            break;
+        } else{
+            $saleDate = $saleDate + 60 * 60 * 24;
+        }
+    }
 
-	$data=[];
-
-    // Render index view
+    $data["bentoArray"] = $bentoArray;
+    $data["saleDateArray"] = [];
+    foreach ($data["bentoArray"] as &$b){
+        $b["startSaleStr"] = date("H:i", $b["start_sale_at"]);
+        $b["saleLengthMinuteOnly"] = (int)(($b["end_sale_at"]-$b["start_sale_at"])/60);
+        $b["orderDeadlineStr"] = date("j日", $b["order_deadline_at"]). "(". DAY_JP[date("w", $b["order_deadline_at"])]. ")". date("H:i", $b["order_deadline_at"]);
+    }
+    $data["saleDateArray"] = [];
+    for ($count = 0; $count <= 10; $count++){
+        $val = strtotime(date("Y-m-d", time() + (60*60*24*$count)));
+        array_push($data["saleDateArray"], ["unix"=> $val, "str"=> date("n月j日", $val). DAY_JP[date("w", $val)]. "曜日"]);
+    }
     return $this->view->render($response, 'top/index.twig', $data);
 });
 
+$app->post('/', function (Request $request, Response $response) {
+    $input = $request->getParsedBody();
+
+    // 削除なら
+    if (!empty($input["deleteTarget"])){
+        return deleteMenuCtrl($response, $this->view, $this->db, $input["deleteTarget"]);
+    
+    // 表示日付変更なら
+    } elseif (!empty($input["showSaleDate"])){
+        return menuManageCtrl($response, $this->view, $this->db, $input["showSaleDate"]);
+    }
+
+});
